bcb25f2b534b0ecd3ab1c000b72b2bfaa864fc31
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-05-15 15:10:15 +0100

    fixed headline test error

--- app/Tests/headline.spec.js
+++ app/Tests/headline.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
-import Headlines from '../components/Headlines';
+import Headlines from '../components/headlines';
 import Signout from '../components/Header';
 
 describe('<HeadLines />', () => {
