8ddc799df3602fe96640cf96aae48bac634eee3c
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-05-30 15:04:37 +0100

    added a newSources spec file

---
+++ app/tests/components/newsSources.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import NewsSources from '../../components/NewsSources';
+
+const props = {
+  sourcesValue: [{
+    "id": "abc-news-au",
+    "name": "ABC News (AU)",
+    "description": "Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.",
+    "url": "http://www.abc.net.au/news",
+    "category": "general",
+    "language": "en",
+    "country": "au",
+    "sortBysAvailable": [
+      "top"
+    ]
+  }],
+};
+
+describe('<NewsSources />', () => {
+  const wrapper = shallow(<NewsSources {...props} />);
+
+  it('Should have a <div> tag that renders the props', () => {
+    expect(wrapper.find('div').children()).to.be.Called;
+  });
+  it('Should have its props rendered on the DOM', () => {
+    expect(wrapper.node.props.children).to.have.length(1);
+  });
+
+});
